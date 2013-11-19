853985361d9cd3c8e19a0802f07d329af04efc90
Abi أب <el@elabee.me>
2013-11-19 20:57:53 +0700

    Initial commit.

---
+++ elabee_facebook/elabee_facebook.js
@@ -0,0 +1,18 @@
+window.fbAsyncInit = function() {
+  FB.init({
+    appId: Drupal.settings.elabeeFacebook.facebookAppId,
+    channelUrl: Drupal.settings.elabeeFacebook.facebookChannelUrl,
+    xfbml: true
+  });
+};
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {
+    return;
+  }
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "//connect.facebook.net/" + Drupal.settings.elabeeFacebook.facebookLocalization + "/all.js";
+  fjs.parentNode.insertBefore(js, fjs);
+})(document, "script", "facebook-jssdk");
